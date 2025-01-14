40763e99be87beaf550a4796eebb023d41769f6a
GamyuX <168780183+GamyuX@users.noreply.github.com>
2025-01-14 16:29:54 -0300

    projeto 15

---
+++ app/src/main/java/com/example/projeto_15/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.projeto_15
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.projeto_15.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        replaceFragment(HomeFragment())
+
+        binding.bottomNavigationView3.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> replaceFragment(HomeFragment())
+                R.id.colections -> replaceFragment(collectionsFragment())
+                R.id.accessibilit -> replaceFragment(AccessibilitFragment())
+                R.id.Exit -> finish()
+                else -> {
+                    replaceFragment(HomeFragment())
+                }
+            }
+            true
+        }
+    }
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout, fragment)
+        fragmentTransaction.commit()
+    }
+}
